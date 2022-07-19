f13bd6cc889014a10082930a635e0d85e180bb8d
Podcast 139 <104439523+podcast139@users.noreply.github.com>
2022-07-19 11:55:48 +0700

    Update yt_download_img.php

--- yt_download_img.php
+++ yt_download_img.php
@@ -12,12 +12,12 @@
   }
 ?>
 <!DOCTYPE html>
-<!-- Coding By CodingNepal - youtube.com/codingnepal -->
+<!-- Coding By Pham Hong Quang -->
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Download YouTube Video Thumbnail | CodingNepal</title>
+  <title>Download YouTube Video Thumbnail | Podcast 139</title>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 </head>
